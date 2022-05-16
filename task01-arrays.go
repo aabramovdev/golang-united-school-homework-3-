db627f6defaf3e480526df908af2eb844c9bc9b2
aabramovdev <aabramov.dev@gmail.com>
2022-05-16 21:09:31 +0500

    My solution of homework-3

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,15 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+	var sum float32
+	var length int
+	for i := 0; i < 15; i++ {
+		if input[i] == 0 {
+			break
+		}
+		sum += input[i]
+		length += 1
+	}
+	result = sum / float32(length)
 	return
 }
